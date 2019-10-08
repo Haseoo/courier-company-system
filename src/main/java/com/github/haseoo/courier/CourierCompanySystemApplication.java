5eb3d5f792910f1fdee1d721aed1f1904e0ce50d
Haseoo <dawid_szymkiewicz@hotmail.com>
2019-10-08 15:07:21 +0200

    Spring Initializr

---
+++ src/main/java/com/github/haseoo/courier/CourierCompanySystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.haseoo.courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierCompanySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierCompanySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/haseoo/courier/CourierCompanySystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.haseoo.courier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourierCompanySystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
