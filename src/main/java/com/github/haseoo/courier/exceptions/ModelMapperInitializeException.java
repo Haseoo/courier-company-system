b857af6a71876ce341d48d53dc3be89a8731951e
Haseoo <dawid_szymkiewicz@hotmail.com>
2019-10-24 13:15:01 +0200

    Fix typo

--- src/main/java/com/github/haseoo/courier/exceptions/ModelMapperInitializeException.java
+++ src/main/java/com/github/haseoo/courier/exceptions/ModelMapperInitializeException.java
@@ -3,7 +3,7 @@ package com.github.haseoo.courier.exceptions;
 import static com.github.haseoo.courier.exceptions.ExceptionMessages.MODEL_MAPPER_INITIALIZE_EXCEPTION;
 
 public class ModelMapperInitializeException extends RuntimeException {
-    public git ModelMapperInitializeException(Throwable cause) {
+    public ModelMapperInitializeException(Throwable cause) {
         super(MODEL_MAPPER_INITIALIZE_EXCEPTION);
         initCause(cause);
     }
