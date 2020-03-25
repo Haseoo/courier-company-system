9b7dac94133d3f0471cb9706b2e8ed9a4ee58b81
Haseoo <dawid_szymkiewicz@hotmail.com>
2020-03-25 17:07:07 +0100

    Spring initializr

---
+++ src/test/java/pl/kielce/tu/weaii/isz/courier/CourierApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kielce.tu.weaii.isz.courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
