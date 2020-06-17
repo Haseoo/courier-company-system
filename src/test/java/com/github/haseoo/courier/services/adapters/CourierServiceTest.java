093416c4695e8730fbf5206aaf99a41dba951cd1
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-17 15:35:16 +0200

    Add courier service tests

--- src/test/java/com/github/haseoo/courier/services/adapters/CourierServiceTest.java
+++ src/test/java/com/github/haseoo/courier/services/adapters/CourierServiceTest.java
@@ -1,12 +1,16 @@
 package com.github.haseoo.courier.services.adapters;
 
+import com.github.haseoo.courier.exceptions.serviceexceptions.userexceptions.InvalidPeselException;
+import com.github.haseoo.courier.exceptions.serviceexceptions.userexceptions.InvalidPeselFormatException;
 import com.github.haseoo.courier.exceptions.serviceexceptions.userexceptions.employees.ActiveCourierExistsException;
 import com.github.haseoo.courier.exceptions.serviceexceptions.userexceptions.employees.EmployeeNotFoundException;
 import com.github.haseoo.courier.models.CourierModel;
+import com.github.haseoo.courier.models.EmployeeModel;
 import com.github.haseoo.courier.repositories.ports.CourierRepository;
 import com.github.haseoo.courier.repositories.ports.EmployeeRepository;
 import com.github.haseoo.courier.servicedata.users.employees.CourierData;
 import com.github.haseoo.courier.servicedata.users.employees.EmployeeAddOperationData;
+import com.github.haseoo.courier.servicedata.users.employees.EmployeeEditOperationData;
 import com.github.haseoo.courier.services.ports.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Tag;
@@ -20,10 +24,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static com.github.haseoo.courier.testutlis.ModelMapperConfig.ModelMapperConfig;
 import static com.github.haseoo.courier.testutlis.constants.Constants.*;
+import static com.github.haseoo.courier.testutlis.constants.UsersConstants.INVALID_PESEL;
+import static com.github.haseoo.courier.testutlis.constants.UsersConstants.INVALID_PESEL_FORMAT;
 import static com.github.haseoo.courier.testutlis.generators.UsersDataGenerator.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
@@ -62,9 +70,9 @@ class CourierServiceTest {
         CourierModel courierModel = getCourierModel();
         when(courierRepository.getById(FIRST_ID)).thenReturn(Optional.of(courierModel));
         //when
-        CourierData c = sut.getById(FIRST_ID);
+        CourierData out = sut.getById(FIRST_ID);
         //then
-        verify(courierRepository).getById(FIRST_ID);
+        Assertions.assertThat(out.getId()).isEqualTo(courierModel.getId());
     }
 
     @Test
@@ -83,7 +91,7 @@ class CourierServiceTest {
         when(passwordEncoder.encode(any())).thenReturn(String.valueOf(in.getPassword()));
         ArgumentCaptor<CourierModel> argument = ArgumentCaptor.forClass(CourierModel.class);
         //when
-        sut.add(in);
+        CourierData out = sut.add(in);
         //then
         verify(courierRepository).saveAndFlush(argument.capture());
         Assertions.assertThat(argument.getValue().getName()).isEqualTo(in.getName());
@@ -91,7 +99,14 @@ class CourierServiceTest {
         Assertions.assertThat(argument.getValue().getPesel()).isEqualTo(in.getPesel());
         Assertions.assertThat(argument.getValue().getPhoneNumber()).isEqualTo(in.getPhoneNumber());
         Assertions.assertThat(argument.getValue().getPassword()).isEqualTo(in.getPassword());
+
+        Assertions.assertThat(out.getName()).isEqualTo(in.getName());
+        Assertions.assertThat(out.getSurname()).isEqualTo(in.getSurname());
+        Assertions.assertThat(out.getPhoneNumber()).isEqualTo(in.getPhoneNumber());
+        Assertions.assertThat(out.getPesel()).isEqualTo(in.getPesel());
+        Assertions.assertThat(out.getActive()).isTrue();
     }
+
 
     @Test
     void should_throw_exception_when_add_two_couriers_with_same_pesels() {
@@ -102,43 +117,82 @@ class CourierServiceTest {
         Assertions.assertThatThrownBy(() -> sut.add(in)).isExactlyInstanceOf(ActiveCourierExistsException.class);
     }
 
-/*TODO edit tests
     @Test
     void should_edit_courier() {
         //given
+        final String newName = "newName";
+        final String newSurname = "newSurname";
         CourierModel courierModel = getCourierModel();
         courierModel.setId(FIRST_ID);
         when(courierRepository.getById(any())).thenReturn(Optional.of(courierModel));
         when(courierRepository.saveAndFlush(any())).thenReturn(getCourierModel());
-        CourierEditOperationData in = getCourierOperationData();
+        EmployeeEditOperationData in = EmployeeEditOperationData.builder()
+                .name(newName)
+                .surname(newSurname)
+                .build();
         ArgumentCaptor<CourierModel> argument = ArgumentCaptor.forClass(CourierModel.class);
         //when
         sut.edit(FIRST_ID, in);
         //then
         verify(courierRepository).saveAndFlush(argument.capture());
         Assertions.assertThat(argument.getValue().getId()).isEqualTo(FIRST_ID);
+        Assertions.assertThat(argument.getValue().getName()).isEqualTo(newName);
+        Assertions.assertThat(argument.getValue().getSurname()).isEqualTo(newSurname);
     }
+
 
     @Test
     void should_throw_exception_when_edit_courier_that_does_not_exits() {
         //given
         when(courierRepository.getById(any())).thenReturn(Optional.empty());
-        CourierEditOperationData in = getCourierOperationData();
+        EmployeeEditOperationData in = EmployeeEditOperationData.builder().build();
         //when & then
         Assertions.assertThatThrownBy(() -> sut.edit(FIRST_ID, in)).isExactlyInstanceOf(EmployeeNotFoundException.class);
     }
 
+
     @Test
-    void should_throw_exception_when_invalid_pesel_format() {
-        //given & when & then
-        Assertions.assertThatThrownBy(() -> sut.add(CourierAddOperationData.builder().pesel(INVALID_PESEL_FORMAT).build()))
+    void should_throw_exception_when_invalid_pesel_format_on_add() {
+        //given
+        EmployeeAddOperationData in = EmployeeAddOperationData.builder()
+                .userName("uName")
+                .password(new char[0])
+                .active(true)
+                .name("")
+                .surname("")
+                .phoneNumber("")
+                .pesel(INVALID_PESEL_FORMAT).build();
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.add(in))
                 .isExactlyInstanceOf(InvalidPeselFormatException.class);
     }
 
     @Test
-    void should_throw_exception_when_invalid_pesel() {
-        //given & when & then
-        Assertions.assertThatThrownBy(() -> sut.add(CourierAddOperationData.builder().pesel(INVALID_PESEL).build()))
+    void should_throw_exception_when_invalid_pesel_on_add() {
+        //given
+        EmployeeAddOperationData in = EmployeeAddOperationData.builder()
+                .userName("uName")
+                .password(new char[0])
+                .active(true)
+                .name("")
+                .surname("")
+                .phoneNumber("")
+                .pesel(INVALID_PESEL).build();
+        //when & then
+        Assertions.assertThatThrownBy(() -> sut.add(in))
                 .isExactlyInstanceOf(InvalidPeselException.class);
-    }*/
+    }
+
+    @Test
+    void should_throw_exception_when_changing_pesel_that_already_exists() {
+        //given
+        CourierModel courierModel = getCourierModel();
+        List<EmployeeModel> outList = Arrays.asList((EmployeeModel) courierModel);
+        when(courierRepository.getById(any())).thenReturn(Optional.of(courierModel));
+        when(employeeRepository.findActiveByPesel(any())).thenReturn(outList);
+        EmployeeEditOperationData in = EmployeeEditOperationData.builder().pesel(INVALID_PESEL_FORMAT).build();
+        //when & then
+        Assertions.assertThatThrownBy(() -> sut.edit(FIRST_ID, in)).isExactlyInstanceOf(ActiveCourierExistsException.class);
+    }
+
 }
