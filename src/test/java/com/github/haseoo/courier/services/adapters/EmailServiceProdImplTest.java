a7f1c918ad7b326bd096c77e16ad2c3520041b94
Dawid <dawid_szymkiewicz@hotmail.com>
2020-06-25 12:22:09 +0200

    Fix email service tests (#103)

--- src/test/java/com/github/haseoo/courier/services/adapters/EmailServiceProdImplTest.java
+++ src/test/java/com/github/haseoo/courier/services/adapters/EmailServiceProdImplTest.java
@@ -1,30 +1,30 @@
 package com.github.haseoo.courier.services.adapters;
 
+import com.github.haseoo.courier.models.MailModel;
 import com.github.haseoo.courier.servicedata.parcels.ParcelData;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.mail.MessagingException;
+import java.util.Map;
 
 import static com.github.haseoo.courier.testutlis.constants.Constants.TEST_SEND_EMAIL_ADDRESS;
 import static com.github.haseoo.courier.testutlis.constants.Constants.UNIT_TEST;
 import static com.github.haseoo.courier.testutlis.generators.ParcelDataGenerator.getParcelAtSender;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @Tag(UNIT_TEST)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EmailServiceProdImplTest {
     @Mock
     private EmailSenderServiceImpl emailService;
@@ -32,15 +32,14 @@ class EmailServiceProdImplTest {
     @Mock
     private EmailNotificationServiceImpl emailNotificationService;
 
-    @Spy
-    private ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+    @Mock
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @InjectMocks
     private EmailServiceProdImpl sut;
 
     @BeforeEach
     void beforeEach() {
-        threadPoolTaskExecutor.initialize();
         ReflectionTestUtils.setField(sut,
                 EmailServiceProdImpl.class,
                 "mailFrom",
@@ -52,34 +51,70 @@ class EmailServiceProdImplTest {
     @Test
     void should_send_notification_to_sender() throws MessagingException {
         //given
-        doCallRealMethod().when(threadPoolTaskExecutor).execute(any());
+        doNothing().when(threadPoolTaskExecutor).execute(any());
         ParcelData in = ParcelData.of(getParcelAtSender());
+        ArgumentCaptor<Runnable> argumentCaptor = ArgumentCaptor.forClass(Runnable.class);
         //when
         sut.sentNotificationToSender(in);
+        verify(threadPoolTaskExecutor).execute(argumentCaptor.capture());
+        argumentCaptor.getValue().run();
         //then
-        verify(threadPoolTaskExecutor).execute(any());
+        verify(emailNotificationService).sendNotificationToReceiver(eq(in), anyString());
+        verify(emailService).sendEmail(any());
     }
 
     @Test
     void should_send_notification_to_receiver() throws MessagingException, InterruptedException {
         //given
-        doCallRealMethod().when(threadPoolTaskExecutor).execute(any());
+        doNothing().when(threadPoolTaskExecutor).execute(any());
         ParcelData in = ParcelData.of(getParcelAtSender());
+        ArgumentCaptor<Runnable> argumentCaptor = ArgumentCaptor.forClass(Runnable.class);
+        ArgumentCaptor<MailModel> argument = ArgumentCaptor.forClass(MailModel.class);
+
         //when
         sut.sentNotificationToReceiver(in);
+        verify(threadPoolTaskExecutor).execute(argumentCaptor.capture());
+        argumentCaptor.getValue().run();
         //then
-        verify(threadPoolTaskExecutor).execute(any());
+
+        verify(emailService).sendEmail(argument.capture());
+        Assertions.assertThat(argument.getValue().getFrom()).isEqualTo(TEST_SEND_EMAIL_ADDRESS);
+        Assertions.assertThat(argument.getValue().getMailTo()).isEqualTo(in.getReceiverContactData().getEmailAddress());
+        Assertions.assertThat(argument.getValue().getSubject()).contains("confirmation");
+
+        Map<String, Object> props = argument.getValue().getProps();
+        Assertions.assertThat(props).containsKeys("information", "name", "parcelId", "showPin", "pin");
+        Assertions.assertThat(props.get("name")).isEqualTo(in.getReceiverContactData().getName());
+        Assertions.assertThat(props.get("parcelId")).isEqualTo(in.getId());
+        Assertions.assertThat(props.get("showPin")).isEqualTo("active");
+        Assertions.assertThat(props.get("pin")).isEqualTo(String.valueOf(in.getPin()));
     }
 
     @Test
     void should_send_return_notification() throws MessagingException {
         //given
-        doCallRealMethod().when(threadPoolTaskExecutor).execute(any());
+        doNothing().when(threadPoolTaskExecutor).execute(any());
         ParcelData in = ParcelData.of(getParcelAtSender());
+        ArgumentCaptor<Runnable> argumentCaptor = ArgumentCaptor.forClass(Runnable.class);
+        ArgumentCaptor<MailModel> argument = ArgumentCaptor.forClass(MailModel.class);
         //when
         sut.sentReturnNotification(in);
+        verify(threadPoolTaskExecutor).execute(argumentCaptor.capture());
+        argumentCaptor.getValue().run();
         //then
-        verify(threadPoolTaskExecutor).execute(any());
+
+        verify(emailService).sendEmail(argument.capture());
+        Assertions.assertThat(argument.getValue().getFrom()).isEqualTo(TEST_SEND_EMAIL_ADDRESS);
+        Assertions.assertThat(argument.getValue().getMailTo()).isEqualTo(in.getSender().getEmailAddress());
+        Assertions.assertThat(argument.getValue().getSubject()).contains("return");
+
+        Map<String, Object> props = argument.getValue().getProps();
+        Assertions.assertThat(props).containsKeys("information", "name", "parcelId", "showPin", "pin");
+        Assertions.assertThat(props.get("name")).isEqualTo(in.getSender().getUserName());
+        Assertions.assertThat(props.get("parcelId")).isEqualTo(in.getId());
+        Assertions.assertThat(props.get("information").toString()).contains("return");
+        Assertions.assertThat(props.get("showPin")).isEqualTo("active");
+        Assertions.assertThat(props.get("pin")).isEqualTo(String.valueOf(in.getPin()));
     }
 
 }
